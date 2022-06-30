e31316b82a823776b4145da8d48c7a5c35997804
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-07-01 01:47:58 +0700

    feat(simulation-1): implement DetailTaskActivity

--- ToDoList/app/src/main/java/com/dicoding/todoapp/ui/detail/DetailTaskActivity.kt
+++ ToDoList/app/src/main/java/com/dicoding/todoapp/ui/detail/DetailTaskActivity.kt
@@ -1,16 +1,47 @@
 package com.dicoding.todoapp.ui.detail
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.dicoding.todoapp.R
+import com.dicoding.todoapp.ui.ViewModelFactory
+import com.dicoding.todoapp.utils.TASK_ID
 
 class DetailTaskActivity : AppCompatActivity() {
+
+    private var taskID: Int = -1
+    private lateinit var detailTaskViewModel: DetailTaskViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_detail)
 
-        //TODO 11 : Show detail task and implement delete action
+        //DONE 11 : Show detail task and implement delete action
+        taskID = intent.getIntExtra(TASK_ID, -1)
+        if (taskID == -1) finish()
 
+        val titleTv = findViewById<TextView>(R.id.detail_ed_title)
+        val descriptionTv = findViewById<TextView>(R.id.detail_ed_description)
+        val dateTv = findViewById<TextView>(R.id.detail_ed_due_date)
+
+        val factory = ViewModelFactory.getInstance(this)
+        detailTaskViewModel = ViewModelProvider(this, factory)[DetailTaskViewModel::class.java]
+        detailTaskViewModel.setTaskId(taskID)
+
+        detailTaskViewModel.task.observe(this) {
+            titleTv.text = it?.title
+            descriptionTv.text = it?.description
+            dateTv.text = it?.dueDateMillis.toString()
+        }
+
+        val deleteBtn = findViewById<Button>(R.id.btn_delete_task)
+        deleteBtn.setOnClickListener {
+            detailTaskViewModel.deleteTask()
+            Toast.makeText(this, "Task is Deleted", Toast.LENGTH_SHORT).show()
+            super.onBackPressed()
+        }
     }
 }
