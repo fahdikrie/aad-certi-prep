416d1f5be49a75ae5c7e0deda4874bb74c43d879
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-06-30 13:15:25 +0700

    feat(simulation-1): add task entity & dao declaration

--- ToDoList/app/src/main/java/com/dicoding/todoapp/data/TaskDao.kt
+++ ToDoList/app/src/main/java/com/dicoding/todoapp/data/TaskDao.kt
@@ -2,23 +2,31 @@ package com.dicoding.todoapp.data
 
 import androidx.lifecycle.LiveData
 import androidx.paging.DataSource
+import androidx.room.*
 import androidx.sqlite.db.SupportSQLiteQuery
 
-//TODO 2 : Define data access object (DAO)
+//DONE 2 : Define data access object (DAO)
+@Dao
 interface TaskDao {
-
+    @Query("SELECT * from tasks")
     fun getTasks(query: SupportSQLiteQuery): DataSource.Factory<Int, Task>
 
+    @Query("SELECT * from tasks WHERE id = :taskId")
     fun getTaskById(taskId: Int): LiveData<Task>
 
+    @Query("SELECT * from tasks ORDER BY dueDate ASC")
     fun getNearestActiveTask(): Task
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertTask(task: Task): Long
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertAll(vararg tasks: Task)
 
+    @Delete
     suspend fun deleteTask(task: Task)
 
+    @Query("UPDATE tasks SET completed = :completed WHERE id = :taskId")
     suspend fun updateCompleted(taskId: Int, completed: Boolean)
     
 }
