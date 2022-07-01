442308b7d55ce998eb6a66f15bc737ee17630b57
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-07-01 15:09:29 +0700

    feat(simulation-2): implement settings; notification & theme prefs

--- CourseScheduleApp/app/src/main/java/com/dicoding/courseschedule/notification/DailyReminder.kt
+++ CourseScheduleApp/app/src/main/java/com/dicoding/courseschedule/notification/DailyReminder.kt
@@ -1,13 +1,23 @@
 package com.dicoding.courseschedule.notification
 
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import com.dicoding.courseschedule.R
 import com.dicoding.courseschedule.data.Course
 import com.dicoding.courseschedule.data.DataRepository
-import com.dicoding.courseschedule.util.executeThread
+import com.dicoding.courseschedule.ui.home.HomeActivity
+import com.dicoding.courseschedule.util.*
+import java.util.*
 
 class DailyReminder : BroadcastReceiver() {
 
@@ -22,22 +32,107 @@ class DailyReminder : BroadcastReceiver() {
         }
     }
 
-    //TODO 12 : Implement daily reminder for every 06.00 a.m using AlarmManager
+    //DONE 12 : Implement daily reminder for every 06.00 a.m using AlarmManager
+    @RequiresApi(Build.VERSION_CODES.M)
     fun setDailyReminder(context: Context) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, DailyReminder::class.java)
 
+        val timeArray = REMINDER_TIME.split(":".toRegex())
+            .dropLastWhile { it.isEmpty() }
+            .toTypedArray()
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]))
+            set(Calendar.MINUTE, Integer.parseInt(timeArray[1]))
+            set(Calendar.SECOND, 0)
+        }
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            ID_REPEATING,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        alarmManager.setInexactRepeating(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY,
+            pendingIntent
+        )
+
+        Toast.makeText(
+            context,
+            "Reminder is now set at $REMINDER_TIME everyday",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     fun cancelAlarm(context: Context) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
+        val intent = Intent(context, DailyReminder::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            ID_REPEATING,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        pendingIntent.cancel()
+
+        alarmManager.cancel(pendingIntent)
+
+        Toast.makeText(
+            context,
+            "Reminder is now cancelled",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
+    @SuppressLint("UnspecifiedImmutableFlag")
     private fun showNotification(context: Context, content: List<Course>) {
-        //TODO 13 : Show today schedules in inbox style notification & open HomeActivity when notification tapped
+        //DONE 13 : Show today schedules in inbox style notification & open HomeActivity when notification tapped
         val notificationStyle = NotificationCompat.InboxStyle()
         val timeString = context.resources.getString(R.string.notification_message_format)
         content.forEach {
             val courseData = String.format(timeString, it.startTime, it.endTime, it.courseName)
             notificationStyle.addLine(courseData)
         }
+
+        val intent = Intent(context, HomeActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            NOTIFICATION_ID,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notification: NotificationCompat.Builder = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setStyle(notificationStyle)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle(context.getString(R.string.today_schedule))
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setAutoCancel(true)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                NOTIFICATION_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notification.setChannelId(NOTIFICATION_CHANNEL_ID)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(NOTIFICATION_ID, notification.build())
+    }
+
+    companion object {
+        private const val REMINDER_TIME = "06:00"
     }
 }
--- CourseScheduleApp/app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
+++ CourseScheduleApp/app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
@@ -2,15 +2,42 @@ package com.dicoding.courseschedule.ui.setting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.dicoding.courseschedule.R
+import com.dicoding.courseschedule.notification.DailyReminder
+import com.dicoding.courseschedule.util.NightMode
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
+    private lateinit var dailyReminder: DailyReminder
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        //TODO 10 : Update theme based on value in ListPreference
-        //TODO 11 : Schedule and cancel notification in DailyReminder based on SwitchPreference
+        //DONE 10 : Update theme based on value in ListPreference
+        val themePreferences = findPreference<ListPreference>(getString(R.string.pref_key_dark))
+        themePreferences?.setOnPreferenceChangeListener { _, newValue ->
+            when (newValue) {
+                "on" -> updateTheme(NightMode.ON.value)
+                "off" -> updateTheme(NightMode.OFF.value)
+                "auto" -> updateTheme(NightMode.AUTO.value)
+                else -> {}
+            }
+            return@setOnPreferenceChangeListener true
+        }
+
+        //DONE 11 : Schedule and cancel notification in DailyReminder based on SwitchPreference
+        dailyReminder = DailyReminder()
+        val notificationPreferences = findPreference<SwitchPreference>(getString(R.string.pref_key_notify))
+        notificationPreferences?.setOnPreferenceChangeListener { _, newValue ->
+            when (newValue) {
+                true -> context?.let { dailyReminder.setDailyReminder(it) }
+                false -> context?.let { dailyReminder.cancelAlarm(it) }
+                else -> {}
+            }
+            return@setOnPreferenceChangeListener true
+        }
     }
 
     private fun updateTheme(nightMode: Int): Boolean {
