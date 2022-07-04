044153c352e76bbe1a5fc0ac34dac475bb8e163a
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-07-04 19:04:24 +0700

    fix(simulation-1): add test for TaskActivity

---
+++ ToDoList/app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
@@ -0,0 +1,31 @@
+package com.dicoding.todoapp.ui.list
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import com.dicoding.todoapp.R
+import com.dicoding.todoapp.ui.add.AddTaskActivity
+import org.junit.Before
+import org.junit.Test
+
+class HomeActivityTest {
+    // Reference:
+    // https://stackoverflow.com/questions/25998659/espresso-how-can-i-check-if-an-activity-is-launched-after-performing-a-certain
+    @Before
+    fun setUp(){
+        ActivityScenario.launch(TaskActivity::class.java)
+    }
+
+    @Test
+    fun assertFabToAddTaskIsWorking(){
+        Intents.init()
+        onView(ViewMatchers.withId(R.id.fab))
+            .perform(ViewActions.click())
+        intended(IntentMatchers.hasComponent(AddTaskActivity::class.java.name))
+        Intents.release()
+    }
+}
